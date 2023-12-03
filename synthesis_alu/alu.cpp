9f1f0be2d8727db938b1b2c0e649bb9a00cbf04b
Jonathan Hager <mail@hager-jonathan.de>
2023-12-03 11:10:56 +0100

    Working alu with small testbench

--- synthesis_alu/alu.cpp
+++ synthesis_alu/alu.cpp
@@ -1,21 +1,76 @@
+#include <iostream>
 #include <systemc.h>
 
+using namespace std;
+
 SC_MODULE(alu) {
-  sc_in<bool> clk;
-  sc_in<bool> reset;
-  sc_in<sc_bv<32>> rs1;
-  sc_in<sc_bv<32>> rs2;
-  sc_in<sc_bv<12>> imm;
-  // HACK: 17 ist blöde Größe
-  sc_in<sc_bv<17>> instruction;
-  sc_in<sc_bv<32>> rd;
+    sc_in<bool> clk;
+    sc_in<bool> reset;
+    sc_in<sc_bv<32>> rs1;
+    sc_in<sc_bv<32>> rs2;
+    sc_in<sc_bv<12>> imm;
+    sc_in<sc_bv<17>> instruction;
 
-  void do_alu() {}
+    sc_out<sc_bv<32>> rd;
 
-  SC_CTOR(alu)
-      : clk("clk"), reset("reset"), rs1("rs1"), rs2("rs2"), imm("imm"),
-        instruction("instruction"), rd("rd") {
-    SC_METHOD(do_alu);
-    sensitive << clk << reset;
-  }
+    void do_alu() {
+        while (true) {
+            wait();
+            if (reset) {
+                rd.write(0);
+            }
+
+            sc_bv<7> opcode = instruction.read().range(6, 0);
+            sc_bv<10> funct73 = instruction.read().range(16, 7);
+            if (opcode == 0b0110011) {         // R-Type
+                if (funct73 == 0b0000000000) { // ADD
+                    rd.write(rs1.read().to_int() + rs2.read().to_int());
+
+                } else if (funct73 == 0b0000000111) { // AND
+                    rd.write(rs1.read().to_uint() & rs2.read().to_uint());
+
+                } else if (funct73 == 0b0000001000) { // MUL
+                    rd.write(rs1.read().to_int() * rs2.read().to_int());
+
+                } else if (funct73 == 0b0000000001) { // SLL
+                    rd.write(rs1.read().to_int()
+                             << rs2.read().range(4, 0).to_uint());
+
+                } else if (funct73 == 0b0100000101) { // SRA
+                    rd.write(rs1.read().to_int() >>
+                             rs2.read().range(4, 0).to_uint());
+
+                } else {
+                    rd.write(0);
+                }
+            } else if (opcode == 0b0010011) { // ADDI
+                rd.write(rs1.read().to_int() + imm.read().to_int());
+            } else if (opcode == 0b0000011) { // LW
+                rd.write(rs1.read().to_int() + imm.read().to_int());
+            } else if (opcode == 0b0100011) { // SW
+                rd.write(rs1.read().to_int() + imm.read().to_int());
+            } else if (opcode == 0b1100011) { // BNE
+                sc_bv<32> temp;
+                temp.range(31, 2) = 0;
+
+                if (rs1.read() == rs2.read()) {
+                    temp.range(1, 0) = 0b10;
+                } else {
+                    temp.range(1, 0) = 0b00;
+                }
+
+                rd.write(temp);
+            } else {
+                rd.write(0);
+            }
+        }
+    }
+
+    SC_CTOR(alu)
+        : clk("clk"), reset("reset"), rs1("rs1"), rs2("rs2"), imm("imm"),
+          instruction("instruction"), rd("rd") {
+        SC_THREAD(do_alu);
+        sensitive << clk.pos();
+        async_reset_signal_is(reset, true);
+    }
 };
--- synthesis_alu/testbench.cpp
+++ synthesis_alu/testbench.cpp
@@ -1,37 +1,246 @@
 /******************************************************************************
  * Copyright (c) 2020, Intel Corporation. All rights reserved.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception.
- * 
+ *
  *****************************************************************************/
 
 // Design template
 
-#include <systemc.h>
 #include "alu.cpp"
- 
-// ICSC requires DUT top should be instantiated inside wrapper (typically TB) 
+#include <systemc.h>
+
+// ICSC requires DUT top should be instantiated inside wrapper (typically TB)
 // and all DUT ports are bound.
-struct Tb : sc_module 
-{
-    sc_signal<bool> a{"a"};
-    
-    sc_signal<bool> b{"b"};
-    sc_signal<bool> z{"z"};
- 
+struct Tb : sc_module {
+    sc_in<bool> clk;
+    sc_signal<bool> reset;
+    sc_signal<sc_bv<32>> rs1;
+    sc_signal<sc_bv<32>> rs2;
+    sc_signal<sc_bv<12>> imm;
+    sc_signal<sc_bv<17>> instruction;
+
+    sc_signal<sc_bv<32>> rd;
+
     alu dut_inst{"dut_inst"};
 
-    SC_CTOR(Tb) 
-    {
-        dut_inst.A(a);
-        dut_inst.B(b);
-        dut_inst.Z(z);
+    SC_CTOR(Tb) {
+        dut_inst.clk(clk);
+        dut_inst.reset(reset);
+        dut_inst.rs1(rs1);
+        dut_inst.rs2(rs2);
+        dut_inst.imm(imm);
+        dut_inst.instruction(instruction);
+        dut_inst.rd(rd);
+
+        SC_THREAD(test);
+    }
+
+    void test() {
+        const int OP_ADD = 0b00000000000110011;
+        const int OP_AND = 0b00000001110110011;
+        const int OP_MUL = 0b00000010000110011;
+        const int OP_SLL = 0b00000000010110011;
+        const int OP_SRA = 0b01000001010110011;
+        const int OP_ADDI = 0b00000000000010011;
+        const int OP_LW = 0b00000000000000011;
+        const int OP_SW = 0b00000000000100011;
+        const int OP_BNE = 0b00000000001100011;
+
+        reset.write(false);
+
+        // ============================ MUL ============================
+        rs1.write(20);
+        rs2.write(2);
+        imm.write(0xFFF);
+        instruction.write(OP_MUL);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " * " << rs2.read().to_int() << " = "
+             << rd.read().to_int() << endl;
+
+        rs1.write(20);
+        rs2.write(-100);
+        imm.write(0xFFF);
+        instruction.write(OP_MUL);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " * " << rs2.read().to_int() << " = "
+             << rd.read().to_int() << endl;
+
+        rs1.write(65535); // overflow after MUL
+        rs2.write(65536);
+        imm.write(0xFFF);
+        instruction.write(OP_MUL);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " * " << rs2.read().to_int() << " = "
+             << rd.read().to_int() << endl;
+
+        rs1.write(65536); // overflow after MUL
+        rs2.write(65536);
+        imm.write(0xFFF);
+        instruction.write(OP_MUL);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " * " << rs2.read().to_int() << " = "
+             << rd.read().to_int() << endl;
+
+        // ============================ ADD ============================
+        rs1.write(100);
+        rs2.write(27);
+        imm.write(0xFFF);
+        instruction.write(OP_ADD);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " + " << rs2.read().to_int() << " = "
+             << rd.read().to_int() << endl;
+
+        rs1.write(110);
+        rs2.write(-27);
+        imm.write(0xFFF);
+        instruction.write(OP_ADD);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " + " << rs2.read().to_int() << " = "
+             << rd.read().to_int() << endl;
+
+        rs1.write(0);
+        rs2.write(-2);
+        imm.write(0xFFF);
+        instruction.write(OP_ADD);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " + " << rs2.read().to_int() << " = "
+             << rd.read().to_int() << endl;
+
+        rs1.write(-2147483648);
+        rs2.write(-1);
+        imm.write(0xFFF);
+        instruction.write(OP_ADD);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " + " << rs2.read().to_int() << " = "
+             << rd.read().to_int() << endl;
+
+        // ============================ SLL ============================
+        rs1.write(20);
+        rs2.write(2);
+        imm.write(0xFFF);
+        instruction.write(OP_SLL);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " << " << rs2.read().to_int() << " = "
+             << rd.read().to_int() << endl;
+
+        rs1.write(1);
+        rs2.write(30);
+        imm.write(0xFFF);
+        instruction.write(OP_SLL);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " << " << rs2.read().to_uint() << " = "
+             << rd.read().to_int() << endl;
+
+        rs1.write(1);
+        rs2.write(31);
+        imm.write(0xFFF);
+        instruction.write(OP_SLL);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " << " << rs2.read().to_int() << " = "
+             << rd.read().to_int() << endl;
+
+        rs1.write(1);
+        rs2.write(32);
+        imm.write(0xFFF);
+        instruction.write(OP_SLL);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " << " << rs2.read().to_int() << " = "
+             << rd.read().to_int() << endl;
+
+        // ============================ SRA ============================
+        rs1.write(-17);
+        rs2.write(1);
+        imm.write(0xFFF);
+        instruction.write(OP_SRA);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " >> " << rs2.read().to_int() << " = "
+             << rd.read().to_int() << endl;
+
+        rs1.write(40);
+        rs2.write(2);
+        imm.write(0xFFF);
+        instruction.write(OP_SRA);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " >> " << rs2.read().to_int() << " = "
+             << rd.read().to_int() << endl;
+
+        rs1.write(65536);
+        rs2.write(10);
+        imm.write(0xFFF);
+        instruction.write(OP_SRA);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " >> " << rs2.read().to_int() << " = "
+             << rd.read().to_int() << endl;
+
+        // ============================ AND ============================
+        rs1.write(20);
+        rs2.write(17);
+        imm.write(0xFFF);
+        instruction.write(OP_AND);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read() << " & " << rs2.read() << " = " << rd.read() << endl;
+
+        rs1.write(0b10101010101010101010101010101010);
+        rs2.write(0b01010101010101010101010101010101);
+        imm.write(0xFFF);
+        instruction.write(OP_AND);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read() << " & " << rs2.read() << " = " << rd.read() << endl;
+
+        rs1.write(0xABCDEF01);
+        rs2.write(0xABCDEF01);
+        imm.write(0xFFF);
+        instruction.write(OP_AND);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read() << " & " << rs2.read() << " = " << rd.read() << endl;
+
+        // ============================ BNE ============================
+        rs1.write(10);
+        rs2.write(10);
+        imm.write(0xFFF);
+        instruction.write(OP_BNE);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " == " << rs2.read().to_int() << ": "
+             << (bool)rd.read().to_int() << endl;
+
+        rs1.write(10);
+        rs2.write(11);
+        imm.write(0xFFF);
+        instruction.write(OP_BNE);
+        wait(sc_time(20, SC_NS));
+        cout << sc_time_stamp() << ": ";
+        cout << rs1.read().to_int() << " == " << rs2.read().to_int() << ": "
+             << (bool)rd.read().to_int() << endl;
+
+        sc_stop();
     }
 };
- 
-int sc_main (int argc, char **argv)
-{
-     Tb tb("tb");
-     sc_start();
-     return 0;
+
+int sc_main(int argc, char **argv) {
+    Tb tb("tb");
+    sc_clock my_clock("clock", 10, SC_NS, 0.5, 0, SC_NS, false);
+    tb.clk(my_clock);
+    sc_start();
+
+    return 0;
 }
